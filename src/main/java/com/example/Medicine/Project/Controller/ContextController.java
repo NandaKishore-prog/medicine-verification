5a4a4ebcbdb80f154be11e2e7f3e6bdbf39cdda6
NandaKishore-prog <nanda.kishore@gmail.com>
2025-07-20 20:03:40 +0530

    updated yml

--- src/main/java/com/example/Medicine/Project/Controller/ContextController.java
+++ src/main/java/com/example/Medicine/Project/Controller/ContextController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ContextController {
 
-    @GetMapping({"/index","/"})
+    @GetMapping("/")
     public String IndexPage(){
         return "/index";
     }
